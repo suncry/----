8d880d83f7a2850f3ed56543560f712788962e58
Insane <luojieyy@gmail.com>
2013-06-24 13:32:47 +0800

    Initial Commit

---
+++ Clock/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Clock
+//
+//  Created by Suncry on 13-6-24.
+//  Copyright (c) 2013年 ipointek. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
